d1c2be6ee7a4b0e4026ef9ff2dea59a00b66c94f
Yegor Sukhorukov <egorsukhorukov@mail.ru>
2022-05-03 00:17:42 +0300

    init

---
+++ src/main/java/ru/yegor/pastabox/PastaboxApplication.java
@@ -0,0 +1,13 @@
+package ru.yegor.pastabox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastaboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastaboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/yegor/pastabox/PastaboxApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yegor.pastabox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastaboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
